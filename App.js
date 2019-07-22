b4b47c5187c5eb8ac5831c2846a928c92195f7cc
PxllPie <adwinugroho1@gmail.com>
2019-07-22 22:45:05 +0700

    add Main screen

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
-import AppNavigation from './src/route/AppNavigator';
-// import store from './src/Public/redux/store';
-// import {Provider} from 'react-redux';
+import React, { Component } from 'react';
+
+
+import Main from './src/screens/Main'
 
 
 export default class App extends Component{
   render() {
     return (
-      // <Provider store={store}>
-        
-        <AppNavigation
-        />
-      // </Provider>
+
+        <Main />
+
     )
   }
 }
