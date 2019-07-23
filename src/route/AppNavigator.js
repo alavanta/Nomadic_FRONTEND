a32903cb96f13084926d7d2955877557a1c31c26
reserviert97 <nurlatif97@outlook.com>
2019-07-23 14:00:20 +0700

    connect login screen to backend endpoint

--- src/route/AppNavigator.js
+++ src/route/AppNavigator.js
@@ -1,50 +1,51 @@
-import React,{Component} from 'react';
-import { createSwitchNavigator,createStackNavigator,createAppContainer } from 'react-navigation';
+import React, { Component } from 'react';
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation';
 import Main from '../screens/Main';
 import AuthLoading from '../screens/AuthLoading';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
-import Home from '../screens/Home'
+import Home from '../screens/Home';
 import Profile from '../screens/tabmenu/Profile';
-import ChangePassword from '../screens//account/ChangePassword';
-import EditProfile from '../screens/account/EditProfile';
-import Notification from '../screens/account/Notifications';
+import ChangePassword from '../screens/Account/ChangePassword';
+import EditProfile from '../screens/Account/EditProfile';
+import Notification from '../screens/Account/Notifications';
 
-
-const AppStack = createStackNavigator (
-    { 
-        Home: Home,
-        Profile: Profile,
-        ChangePassword: ChangePassword,
-        EditProfile: EditProfile,
-        Notification: Notification
-        
-    },
-    {
-        headerMode:"none"
-    }
+const AppStack = createStackNavigator(
+  {
+    Home: Home,
+    Profile: Profile,
+    ChangePassword: ChangePassword,
+    EditProfile: EditProfile,
+    Notification: Notification
+  },
+  {
+    headerMode: 'none'
+  }
 );
-const AuthStack = createStackNavigator (
-    {
-        Main: Main,
-        Login: Login,
-        Register: Register,
-    },
-    {
-        headerMode:"none"
-    }
+const AuthStack = createStackNavigator(
+  {
+    Main: Main,
+    Login: Login,
+    Register: Register
+  },
+  {
+    headerMode: 'none'
+  }
 );
 
-
-const switchNavigator = createSwitchNavigator (
-    {
-        AuthLoading: AuthLoading,
-        App: AppStack,
-        Auth: AuthStack,
-    },
-    {
-        initialRouteName: 'AuthLoading'
-    }
+const switchNavigator = createSwitchNavigator(
+  {
+    AuthLoading: AuthLoading,
+    App: AppStack,
+    Auth: AuthStack
+  },
+  {
+    initialRouteName: 'AuthLoading'
+  }
 );
 
 export default createAppContainer(switchNavigator);
